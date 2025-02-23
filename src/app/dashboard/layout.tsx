bbd4020a75d74b5a0763a9bdbe375fa992644629
unknown <sarmadjkl@gmail.com>
2025-02-23 17:00:12 +0500

    changes

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,74 +1,69 @@
+"use client"
 import { Header } from "@/components/dashboard/header";
 import LeftSidebar from "@/components/dashboard/LeftSidebar";
-import { getProjects } from "@/lib/actions/projects";
-import { getAllTeams, getTimeTrackingStats } from "@/lib/actions/reports";
-import { getManagerTeams, getTeams } from "@/lib/actions/teams";
-import { getUser } from "@/lib/actions/user";
 import queryClient from "@/lib/tanstack/queryClient";
-import { createClient } from "@/lib/supabase/server";
 import { redirect } from "next/navigation";
+import { useUser } from "@/hooks/use-user";
+import { useEffect } from "react";
+import { RealtimeManager } from "@/lib/realtime";
+import { toast } from "sonner";
 
 // export const dynamic = 'force-dynamic';
 
-export default async function DashboardLayout({
+export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const supabase = await createClient();
 
   try {
-    const {
-      data: { session },
-    } = await supabase.auth.getSession();
+    const { user } = useUser()
 
-    if (!session) {
-      return redirect("/auth/login");
+    if (!user) {
+      return redirect("/auth/sign-in");
     }
-
-    const user = await getUser()
-    // useEffect(() => {
-    //   if (!user?.id) return;
+    useEffect(() => {
+      if (!user?.id) return;
       
-    //   RealtimeManager.subscribeToNotifications(user.id, (notification) => {
-    //     // Show notification toast
-    //     toast(notification.title, {
-    //       description: notification.message,
-    //     });
+      RealtimeManager.subscribeToNotifications(user.id, (notification) => {
+        // Show notification toast
+        toast(notification.title, {
+          description: notification.message,
+        });
 
-    //     // Invalidate relevant queries based on notification type
-    //     switch (notification.type) {
-    //       case "PROJECT_INVITATION":
-    //         queryClient.invalidateQueries({ queryKey: ["projects"] });
-    //         break;
-    //       case "TASK_ASSIGNED":
-    //         queryClient.invalidateQueries({ queryKey: ["tasks"] });
-    //         break;
-    //     }
-    //   });
-
-    //   return () => {
-    //     if (user?.id) {
-    //       RealtimeManager.unsubscribeFromNotifications(user.id);
-    //     }
-    //   };
-    // }, [user?.id]);
-
-    await queryClient.prefetchQuery({ queryKey: ["all-projects"], queryFn: () => getProjects(user?.id as string) })
-    await queryClient.prefetchQuery({ queryKey: ["all-teams"], queryFn: () => getAllTeams()})
-    await queryClient.prefetchQuery({ 
-          queryKey: ["manager-teams"],
-          queryFn: async () => await getManagerTeams(user?.id as string),
+        // Invalidate relevant queries based on notification type
+        switch (notification.type) {
+          case "PROJECT_INVITATION":
+            queryClient.invalidateQueries({ queryKey: ["projects"] });
+            break;
+          case "TASK_ASSIGNED":
+            queryClient.invalidateQueries({ queryKey: ["tasks"] });
+            break;
         }
-    )
-    await queryClient.prefetchQuery({
-      queryKey: ["teams"],
-          queryFn: async () => await getTeams(user?.id as string),
-    })
-    await queryClient.prefetchQuery({
-        queryKey: ["time-tracking-stats"],
-        queryFn: async () => await getTimeTrackingStats(user?.id as string),
-      })
+      });
+
+      return () => {
+        if (user?.id) {
+          RealtimeManager.unsubscribeFromNotifications(user.id);
+        }
+      };
+    }, [user?.id]);
+
+    // await queryClient.prefetchQuery({ queryKey: ["all-projects"], queryFn: () => getProjects(user?.id as string) })
+    // await queryClient.prefetchQuery({ queryKey: ["all-teams"], queryFn: () => getAllTeams()})
+    // await queryClient.prefetchQuery({ 
+    //       queryKey: ["manager-teams"],
+    //       queryFn: async () => await getManagerTeams(user?.id as string),
+    //     }
+    // )
+    // await queryClient.prefetchQuery({
+    //   queryKey: ["teams"],
+    //       queryFn: async () => await getTeams(user?.id as string),
+    // })
+    // await queryClient.prefetchQuery({
+    //     queryKey: ["time-tracking-stats"],
+    //     queryFn: async () => await getTimeTrackingStats(user?.id as string),
+    //   })
 
     return (
       <div
